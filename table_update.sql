85e472ce06a3c5c72f66d75353c3500055fb6a32
kazakalban <kasymsauyt@gmail.com>
2025-10-22 15:38:12 +0500

    чистка и вид  customer_orders_update data
    Updated customer_orders_update table with data transformations and cleaning.

--- table_update.sql
+++ table_update.sql
@@ -1,5 +1,3 @@
-CREATE TABLE public.customer_orders_update AS TABLE public.customer_orders; # Создание резерв копии ориг таблицы
-
 CREATE TABLE public.customer_orders_update AS
     SELECT
         order_id::int AS order_id, 
@@ -13,24 +11,42 @@ CREATE TABLE public.customer_orders_update AS
         order_status, 
         notes
 
-    FROM public.customer_orders;
+    FROM public.customer_orders;                      # Создание резерв копии ориг таблицы
         
+SELECT * FROM public.customer_orders_update;  # Выбор всех данных из обн таблицы
 
 
-SELECT order_id, customer_name, email, order_date, product_name, quantity, price, country, order_status, notes
-FROM public.customer_orders_update;  # Выбор всех данных из обн таблицы
+# customer_name, product_name+, order_status+, country+
+UPDATE public.customer_orders_update
+    SET order_id = REPLACE(order_id, ',', '');        # order_id удалил запятые
+    SET customer_name = INITCAP(customer_name), 
+    product_name = INITCAP(product_name),
+    order_status = INITCAP(order_status), 
+    country = INITCAP(country);                       # customer_name и ... чтобы каждое слово начиналось с заглавной буквы
+    SET email = LOWER(email);                         #  email все буквы строчными
+    SET quantity = 2
+    WHERE quantity = 'two';                           # quantity строки в целые числа
+    SET price = REPLACE(price, ',', '');
+    SET price = REPLACE(price, '$', '');
+    SET price = ROUND(price, 2);                      # price удалил лишное и округлил
+    SET country = 'United States' 
+    WHERE country = 'Usa';
+    SET country = 'United States' 
+    WHERE country = 'Us';
+    SET country = 'United Kingdom'
+    WHERE country = 'Uk';                             # country 'Uk' на 'United Kingdom';
+    SET notes = TRIM(notes);                          # notes удалил пробелы в начале и конце
+
+
+
+
+
+CREATE TABLE public.customer_orders_update AS TABLE public.customer_orders; # Создание резерв копии ориг таблицы
 
 # order_id
 UPDATE public.customer_orders_update 
     SET order_id = REPLACE(order_id, ',', ''); # order_id удалил запятые
-
-# customer_name, product_name+, order_status+, country+
-UPDATE public.customer_orders_update 
-    SET customer_name = INITCAP(customer_name), 
-    product_name = INITCAP(product_name),
-    order_status = INITCAP(order_status), 
-    country = INITCAP(country); # customer_name и ... чтобы каждое слово начиналось с заглавной буквы
-
+        
 # email
 UPDATE public.customer_orders_update 
     SET email = LOWER(email); #  email все буквы строчными
